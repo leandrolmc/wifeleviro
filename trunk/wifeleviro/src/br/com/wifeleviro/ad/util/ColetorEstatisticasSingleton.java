08a5384147c385ca55c8ee4426905f62404bf0cd
felipe.somolinos <felipe.somolinos@d0fdc247-5100-2b84-6bd1-11047659c829>
2010-07-11 19:23:21 +0000

    Coletor parcial, que já pode ser utilizado para o pessoal desenvolver suas partes.

--- trunk/wifeleviro/src/br/com/wifeleviro/ad/util/ColetorEstatisticasSingleton.java
+++ trunk/wifeleviro/src/br/com/wifeleviro/ad/util/ColetorEstatisticasSingleton.java
@@ -1,12 +1,24 @@
 package br.com.wifeleviro.ad.util;
 
+import java.util.Hashtable;
+import java.util.Vector;
+
 // Classe única de coleta dos dados estatísticos para avaliação do programa.
 // O padrão singleton utilizado garante que não exista mais que uma intância 
 //  desta classe dentro do programa em execução. 
 public class ColetorEstatisticasSingleton {
 
+	// Variável que armazenará as estatísticas de cada estação.
+	private Estatisticas[] estatisticas;
+	
 	// Construtor padrão da classe.
 	private ColetorEstatisticasSingleton() {
+		// Inicia uma instância de Estatisticas para cada estação.
+		estatisticas = new Estatisticas[4];
+		
+		for(int i = 0; i < 4; i++){
+			estatisticas[i] = new Estatisticas();
+		}
 	}
 
 	// Variável privada que contém a única referência para o coletor de
@@ -28,6 +40,33 @@ public class ColetorEstatisticasSingleton {
 	// para armazenamento dos dados coletados.
 	private class Estatisticas {
 
+		// Hashtable que irá armazenar as coletas dos tempos iniciais de cada
+		// quadro para medição dos tap´s.
+		protected Hashtable<Long, Double> tapMedicaoInicio;
+		// Lista encadeada não-ordenada que irá armazenar os tap´s medidos.
+		protected Vector<Double> tap;
+		
+		// Hashtable que irá armazenar as coletas dos tempos iniciais de cada
+		// quadro para medição dos tam´s.
+		protected Hashtable<Long, Double> tamMedicaoInicio;
+		// Lista encadeada não-ordenada que irá armazenar os tam´s medidos.
+		protected Vector<Double> tam;
+		
+		// Hashtable indexada pelo identificador da mensagem que armazena
+		// o número de colisões ocorridas na mensagem.
+		protected Hashtable<Long, Long> colisoesPorMensagem;
+		// Hashtable indexada pelo identificador da mensagem que armazena
+		// o número de quadros que foram necessários para transmitir a mensagem.
+		protected Hashtable<Long, Long> quadrosPorMensagem;
+		
+		protected Estatisticas(){
+			tapMedicaoInicio = new Hashtable<Long, Double>();
+			tap = new Vector<Double>();
+			tamMedicaoInicio = new Hashtable<Long, Double>();
+			tam = new Vector<Double>();
+			colisoesPorMensagem = new Hashtable<Long, Long>();
+			quadrosPorMensagem = new Hashtable<Long, Long>();
+		}
 	}
 
 	// Coleta de E[Tap(i)] (tempo de acesso de um quadro na estação i)
@@ -35,8 +74,11 @@ public class ColetorEstatisticasSingleton {
 	// Coleta o valor do tempo quando do início do acesso de um quadro na
 	// estação i. Todo quadro tem um identificador único que irá indexá-lo
 	// na hash de armazenamento do tempo inicial.
-	public void iniciaColetaTap(long idEstacao, long idQuadro, double tempo) {
-
+	public void iniciaColetaTap(int idEstacao, long idQuadro, double tempoInicio) {
+		// Armazena na hashtable tapMedicaoInicio referente a estação
+		// identificada por idEstacao o tempo de início de acesso do 
+		// quadro identificado por idQuadro.
+		this.estatisticas[idEstacao].tapMedicaoInicio.put(idQuadro, tempoInicio);
 	}
 
 	// Coleta o valor do tempo quando do fim do acesso 
@@ -45,41 +87,91 @@ public class ColetorEstatisticasSingleton {
 	// modo a armazenar este em um agregador de "tap´s" da estação i,
 	// perdendo a identificação única do quadro e tornando esta medição
 	// em um "freguês típico" do sistema.
-	public void finalizaColetaTap(long idEstacao, long idQuadro, double tempo) {
-
+	public void finalizaColetaTap(int idEstacao, long idQuadro, double tempoFim) {
+		// Recupero da hashtable o tempo do início do acesso do quadro
+		// identificado por idQuadro.
+		double tempoInicio = (Double)this.estatisticas[idEstacao].tapMedicaoInicio.get(idQuadro);
+		// O cálculo do tempo de acesso é dado simplesmente pela subtração
+		// do instante de tempo inicial do instante de tempo final.
+		double tempoAcesso = tempoFim - tempoInicio;
+		// Armazena na lista encadeada o tempo de acesso do quadro.
+		this.estatisticas[idEstacao].tap.add(tempoAcesso);
 	}
 
 	// Coleta de E[Tam(i)]
 
-	// Coleta o valor do tempo quando do início do acesso de um quadro na
-	// estação i. Todo quadro tem um identificador único que irá indexá-lo
-	// na hash de armazenamento do tempo inicial.
-	public void iniciaColetaTam(long idEstacao, long idQuadro, double tempo) {
-
+	// Coleta o valor do tempo quando do início do acesso do primeiro quadro 
+	// da mensagem a ser transmitida na estação i. Toda mensagem tem um 
+	// identificador único que irá indexá-la na hash de armazenamento 
+	// do tempo inicial.
+	public void iniciaColetaTam(int idEstacao, long idMensagem, double tempoInicio) {
+		// Armazena na hashtable tamMedicaoInicio referente a estação
+		// identificada por idEstacao o tempo de início de acesso do 
+		// primeiro quadro da mensagem identificada por idMensagem.
+		this.estatisticas[idEstacao].tamMedicaoInicio.put(idMensagem, tempoInicio);
 	}
 
-	// Coleta o valor do tempo quando do fim do acesso 
-	// de um quadro na estação i. Busca no hash de armazenamento do tempo
-	// inicial o valor referente ao identificador do quadro informado de
-	// modo a armazenar este em um agregador de "tap´s" da estação i,
-	// perdendo a identificação única do quadro e tornando esta medição
+	// Coleta o valor do tempo quando do fim do acesso do último quadro 
+	// de uma mensagem na estação i. Busca no hash de armazenamento do tempo
+	// inicial o valor referente ao identificador da mensagem informado de
+	// modo a armazenar este em um agregador de "tam´s" da estação i,
+	// perdendo a identificação única da mensagem e tornando esta medição
 	// em um "freguês típico" do sistema.
-	public void finalizaColetaTam(long idEstacao, long idQuadro, double tempo) {
-
+	public void finalizaColetaTam(int idEstacao, long idMensagem, double tempoFim) {
+		// Recupero da hashtable o tempo do início do acesso da mensagem
+		// identificada por idMensagem.
+		double tempoInicio = (Double)this.estatisticas[idEstacao].tamMedicaoInicio.get(idMensagem);
+		// O cálculo do tempo de acesso é dado simplesmente pela subtração
+		// do instante de tempo inicial do instante de tempo final.
+		double tempoAcesso = tempoFim - tempoInicio;
+		// Armazena na lista encadeada o tempo de acesso da mensagem.
+		this.estatisticas[idEstacao].tam.add(tempoAcesso);
 	}
 	
 	// Coleta de E[NCm(i)]
 	
 	// Coleta dados sobre a existência ou não de colisão na transmissão de um
-	// quadro qualquer, identificado a mensagem transmitida e a estação,
-	// de modo que seja possível calcular a média de colisões por mensagem,
-	// por estação.
-	public void coletaColisaoPorQuadro(long idEstacao, long idMensagem, boolean houveColisao){
-		
+	// quadro qualquer, identificando a mensagem transmitida, da qual o quadro 
+	// faz parte, e a estação, de modo que seja possível calcular a média de 
+	// colisões por mensagem, por estação.
+	public void coletaColisaoPorMensagem(int idEstacao, long idMensagem){
+		// Recupera o número de colisões registradas na hashtable para a mensagem
+		// identificada por idMensagem.
+		Long numeroDeColisoes = (Long)this.estatisticas[idEstacao].colisoesPorMensagem.get(idMensagem);
+		// Caso não haja registro na hashtable referente a colisões desta mensagem,
+		// o contador de colisões é iniciado com o valor 0 (ZERO).
+		if(numeroDeColisoes == null){
+			numeroDeColisoes = (long)0;
+		}
+		// Incrementa o número de colisões registradas.
+		numeroDeColisoes = numeroDeColisoes + 1;
+		// Armazena na hashtable as colisões contabilizadas até o momento.
+		this.estatisticas[idEstacao].colisoesPorMensagem.put(idMensagem, numeroDeColisoes);
+	}
+	
+	// Coleta geração de quadro para transmissão de mensagem.
+	// Invocado sempre que há um evento de geração de um quadro para transmissão.
+	public void coletaQuadroPorMensagem(int idEstacao, long idMensagem){
+		// Recupera o número de quadros registrados na hashtable para a mensagem
+		// identificada por idMensagem.
+		Long numeroDeQuadros = (Long)this.estatisticas[idEstacao].quadrosPorMensagem.get(idMensagem);
+		// Caso não haja registro na hashtable referente a quadros desta mensagem,
+		// o contador de quadros é iniciado com o valor 0 (ZERO).
+		if(numeroDeQuadros == null){
+			numeroDeQuadros = (long)0;
+		}
+		// Incrementa o número de quadros registrados.
+		numeroDeQuadros = numeroDeQuadros + 1;
+		// Armazena na hashtable os quadros contabilizados até o momento.
+		this.estatisticas[idEstacao].colisoesPorMensagem.put(idMensagem, numeroDeQuadros);
 	}
 
+	// Coleta de utilização do Ethernet
+	//TODO Ainda não sei como irei implementar a coleta de utilização.
+	
 	// Coleta de vazão(i)
+	//TODO Ainda não sei como irei implementar a vazão.
 
-	// Coleta de utilização do Ethernet com intervalos de confiança de 95%
+
 
 }

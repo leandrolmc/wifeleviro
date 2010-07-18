7305741034c53d5bfe6ce2c0ab567bf2e7e3f1ab
felipe.somolinos <felipe.somolinos@d0fdc247-5100-2b84-6bd1-11047659c829>
2010-07-18 21:43:32 +0000

    Começando a implementar detector de colisões.

--- wifeleviro/src/br/com/wifeleviro/ad/modelo/Evento.java
+++ wifeleviro/src/br/com/wifeleviro/ad/modelo/Evento.java
@@ -3,24 +3,39 @@ package br.com.wifeleviro.ad.modelo;
 public class Evento {
 
 	public static final int GERAR_MENSAGEM = 0;
-	public static final int CHEGADA_QUADRO_NO_RX_HUB = 1;
-	public static final int CHEGADA_QUADRO_NO_RX_TERMINAL = 2;
-	public static final int GERAR_REFORCO_COLISAO = 3;
+	public static final int INICIO_TX_PC = 1;
+	public static final int CHEGADA_QUADRO_NO_RX_HUB = 2;
+	public static final int INICIO_CHEGADA_QUADRO_NO_RX_TERMINAL = 3;
+	public static final int FIM_CHEGADA_QUADRO_NO_RX_TERMINAL = 4;
+	public static final int GERAR_REFORCO_COLISAO = 5;
 	
 	private int tipoEvento;
-	private int idTerminal;
+	private int idTerminalOrigemDaMensagem;
 	
-	public Evento(int idTerminal, int tipoEvento){
-		this.idTerminal = idTerminal;
+	private Integer idTerminalDestino;
+	
+	public Evento(int idTerminalOrigemDaMensagem, int tipoEvento){
+		this.idTerminalOrigemDaMensagem = idTerminalOrigemDaMensagem;
 		this.tipoEvento = tipoEvento;
+		this.idTerminalDestino = null;
 	}
 
-	public int getIdTerminal() {
-		return idTerminal;
+	public Evento(int idTerminalOrigemDaMensagem, int idTerminalDestino, int tipoEvento){
+		this.idTerminalOrigemDaMensagem = idTerminalOrigemDaMensagem;
+		this.tipoEvento = tipoEvento;
+		this.idTerminalDestino = idTerminalDestino;
+	}
+	
+	public int getIdTerminalOrigemDaMensagem() {
+		return idTerminalOrigemDaMensagem;
 	}
 	
 	public int getTipoEvento() {
 		return tipoEvento;
+	}
+
+	public Integer getIdTerminalDestino() {
+		return idTerminalDestino;
 	}
 
 }

3a6e72ff1a9f7b3d9eae875fc62745656f78016c
felipe.somolinos <felipe.somolinos@d0fdc247-5100-2b84-6bd1-11047659c829>
2010-08-04 12:10:25 +0000

    Versão final...

--- trunk/wifeleviro/src/br/com/wifeleviro/ad/Simulador.java
+++ trunk/wifeleviro/src/br/com/wifeleviro/ad/Simulador.java
@@ -1,26 +1,54 @@
 package br.com.wifeleviro.ad;
 
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
 import br.com.wifeleviro.ad.modelo.Terminal;
 
 public class Simulador {
 
 	public static void main(String[] args) {
 
-//		int qtdTerminais = 1;
-		int qtdTerminais = 2;
+		// Para cenários 1 e 2 
+//		int qtdTerminais = 2;
 		
+		// Para cenários 3 e 4
+		int qtdTerminais = 4;
+
+		// Cenário 1
 		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
 		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
-//		Terminal[] terminais = {pc1};
+
+		// Cenário 2
+//		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_EXPONENCIAL, 0.08, 40);
+//		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_EXPONENCIAL, 0.08, 40);
+		
+		// Cenário 3
+//		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
+//		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
+//		Terminal pc3 = new Terminal(2, 60, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
+//		Terminal pc4 = new Terminal(3, 40, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
+
+		// Cenário 4
+//		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
+//		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_EXPONENCIAL, 0.016, 1);
+//		Terminal pc3 = new Terminal(2, 60, Terminal.TIPO_EXPONENCIAL, 0.016, 1);
+//		Terminal pc4 = new Terminal(3, 40, Terminal.TIPO_EXPONENCIAL, 0.016, 1);
+
+
+		// Para cenários 1 e 2 
 		Terminal[] terminais = {pc1, pc2};
+		
+		// Para cenários 3 e 4
+//		Terminal[] terminais = {pc1, pc2, pc3, pc4};
 		
 		Orquestrador orch = new Orquestrador(qtdTerminais, terminais);
 		
-		System.out.println("INICIO DA SIMULACAO");
+		System.out.println("INICIO DA SIMULACAO: "+(new SimpleDateFormat("HH:mm:ss").format(new GregorianCalendar().getTime())));
 		
 		orch.executarSimulacao();
 		
-		System.out.println("FIM DA SIMULACAO");
+		System.out.println("FIM DA SIMULACAO: "+(new SimpleDateFormat("HH:mm:ss").format(new GregorianCalendar().getTime())));
 		
 	}
 

83ea922dcffdf91bd0d3788af2e5d88050ec1127
felipe.somolinos <felipe.somolinos@d0fdc247-5100-2b84-6bd1-11047659c829>
2010-08-04 13:03:42 +0000

    Tinha esquecido de ajustar o finalizador do intervalo de confiança para aceitar 0 < 0,1*0 ser verdadeiro.

--- wifeleviro/src/br/com/wifeleviro/ad/Simulador.java
+++ wifeleviro/src/br/com/wifeleviro/ad/Simulador.java
@@ -16,18 +16,18 @@ public class Simulador {
 		int qtdTerminais = 4;
 
 		// Cenário 1
-		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
-		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
+//		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
+//		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
 
 		// Cenário 2
 //		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_EXPONENCIAL, 0.08, 40);
 //		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_EXPONENCIAL, 0.08, 40);
 		
 		// Cenário 3
-//		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
-//		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
-//		Terminal pc3 = new Terminal(2, 60, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
-//		Terminal pc4 = new Terminal(3, 40, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
+		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
+		Terminal pc2 = new Terminal(1, 80, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
+		Terminal pc3 = new Terminal(2, 60, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
+		Terminal pc4 = new Terminal(3, 40, Terminal.TIPO_DETERMINISTICO, 0.016, 1);
 
 		// Cenário 4
 //		Terminal pc1 = new Terminal(0, 100, Terminal.TIPO_DETERMINISTICO, 0.08, 40);
@@ -37,10 +37,10 @@ public class Simulador {
 
 
 		// Para cenários 1 e 2 
-		Terminal[] terminais = {pc1, pc2};
+//		Terminal[] terminais = {pc1, pc2};
 		
 		// Para cenários 3 e 4
-//		Terminal[] terminais = {pc1, pc2, pc3, pc4};
+		Terminal[] terminais = {pc1, pc2, pc3, pc4};
 		
 		Orquestrador orch = new Orquestrador(qtdTerminais, terminais);
 		

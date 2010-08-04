28ec3ad1c34bb001bbfb154ea78c11006a6d5550
felipe.somolinos <felipe.somolinos@d0fdc247-5100-2b84-6bd1-11047659c829>
2010-08-04 13:03:42 +0000

    Tinha esquecido de ajustar o finalizador do intervalo de confiança para aceitar 0 < 0,1*0 ser verdadeiro.

--- trunk/wifeleviro/src/br/com/wifeleviro/ad/util/estatisticas/IntervaloDeConfianca.java
+++ trunk/wifeleviro/src/br/com/wifeleviro/ad/util/estatisticas/IntervaloDeConfianca.java
@@ -263,8 +263,8 @@ public class IntervaloDeConfianca {
 		
 		boolean dentroDoLimite = (
 //			(tamanhoICTap < (0.1*mediaTap)) &&
-			(tamanhoICTam < (0.1*mediaTam)) &&
-			(tamanhoICNcm < (0.1*mediaNcm)) &&
+			(tamanhoICTam <= (0.1*mediaTam)) &&
+			(tamanhoICNcm <= (0.1*mediaNcm)) &&
 			(tamanhoICUtilizacao <= (0.1*mediaUtilizacao)) &&
 			(tamanhoICVazao < (0.1*mediaVazao))
 		);
